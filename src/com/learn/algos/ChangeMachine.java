87104feb6a97dde53a02a1aedcfb2c948f81855c
Venkatesh Pediredla <venkatesh.pediredla@bt.com>
2021-06-21 16:52:45 +0530

    change machine

---
+++ src/com/learn/algos/ChangeMachine.java
@@ -0,0 +1,35 @@
+package com.learn.algos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChangeMachine {
+    
+    public static void main(String[] args) {
+	
+		
+		int amount = 99;
+		List<Integer> output  = new ArrayList<Integer>();
+		getMinCoins(amount,output);
+		System.out.println(output);
+}
+
+    
+
+    private static List<Integer> getMinCoins(int amount,List<Integer> output) {
+	// TODO Auto-generated method stub
+	int[] coins = {25,10,5,1};
+	for(int i =0;i<coins.length & amount > 0;i++)
+	{
+	    while(amount >= coins[i])
+	    {
+		amount = amount-coins[i];
+		output.add(coins[i]);
+	    }
+	}
+	return output;
+	
+	
+    }
+
+}
